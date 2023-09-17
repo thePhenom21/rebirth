a0cb427db84f588846a3cd7c3f003f260ce548a1
Can Ersoy <can.ersoy@tum.de>
2023-09-17 18:53:31 +0300

    first draft

--- script.sh
+++ script.sh
@@ -1,13 +1,32 @@
 while getopts u:a: flag
 do
     case "${flag}" in
-        u) username=${OPTARG};;
-        a) age=${OPTARG};;
+        u) url=${OPTARG};;
+        a) appname=${OPTARG};;
     esac
 done
 
 
 mkdir builder
 cd $PWD/builder
-echo "<meta http-equiv=\"Refresh\" content=\"0; url='$username'\" />" >> index.html
+cargo create-tauri-app -y $appname
+wait
+cd $appname
+npm install 
+wait
+cd src
+rm -r *
+echo "<meta http-equiv=\"Refresh\" content=\"0; url='$url'\" />" >> index.html
+cd ..
+cd src-tauri
+rm tauri.conf.json
+cd ..
+cd ..
+cd ..
+cp tauri.conf.json $PWD/builder/src-tauri
+cd $PWD/builder/$appname
+npm run tauri build
+wait
+
+
 
